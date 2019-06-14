3b0a3fef5782515d7abdc92037ca7b46d7744a93
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-14 00:05:45 -0600

    updated startup routing to use areas.

--- GraniteWarehouse/Startup.cs
+++ GraniteWarehouse/Startup.cs
@@ -69,8 +69,9 @@ namespace GraniteWarehouse
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
