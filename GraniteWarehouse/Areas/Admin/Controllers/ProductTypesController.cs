b995c2be1360d0d1b8e5b24b822017047b21dbbb
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-15 12:55:28 -0600

    created ProductTypes controller in admin/controller folder to access the database and dependency injection.

---
+++ GraniteWarehouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteWarehouse.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteWarehouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+    }
+}
