4d4a4e633838ff01ddef24b8688f007c41956fe8
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-16 20:40:31 -0600

    created specialtags controller, added readonly file and constructor.

---
+++ GraniteWarehouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteWarehouse.Data;
+using GraniteWarehouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteWarehouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
