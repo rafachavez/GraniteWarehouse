d854fd3bc79a35383124af02427e408b5f485c61
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-16 22:52:07 -0600

    added Products controller, setup dependency injection and initialized the viewmodel

---
+++ GraniteWarehouse/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteWarehouse.Data;
+using GraniteWarehouse.Models;
+using GraniteWarehouse.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteWarehouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db; // this is for dependency injection
+
+        public ProductViewModels ProductsVM { get; set; }
+
+        public ProductsController(ApplicationDbContext db) // contructor
+        {
+            _db = db;
+            ProductsVM = new ProductViewModels() //initialize the viewmodel
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
