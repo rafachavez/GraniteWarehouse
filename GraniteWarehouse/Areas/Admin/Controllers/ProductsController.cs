f421a5333e5e16b60ed2532db7af8ef8466ab50f
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-18 23:48:55 -0600

    updated product controler to add edit action.

--- GraniteWarehouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteWarehouse/Areas/Admin/Controllers/ProductsController.cs
@@ -98,5 +98,24 @@ namespace GraniteWarehouse.Areas.Admin.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        //Get edit
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if(ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductsVM);
+               
+        }
     }
 }
