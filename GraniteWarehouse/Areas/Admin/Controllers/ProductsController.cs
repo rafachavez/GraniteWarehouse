5d563e74bfae0c06b20485b651dbeb333e378c10
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-20 00:15:55 -0600

    added delete action to the products controller

--- GraniteWarehouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteWarehouse/Areas/Admin/Controllers/ProductsController.cs
@@ -187,5 +187,24 @@ namespace GraniteWarehouse.Areas.Admin.Controllers
 
         }
 
+        //Get delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductsVM);
+
+        }
+
     }
 }
