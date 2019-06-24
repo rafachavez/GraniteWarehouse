4c0d237e19d909468fe00ffb83a283b357feddf1
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-24 01:45:59 -0600

    added new table of application users to applicationDbContext

--- GraniteWarehouse/Data/ApplicationDbContext.cs
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -20,5 +20,7 @@ namespace GraniteWarehouse.Data
         public DbSet<SpecialTags> SpecialTags { get; set; }
 
         public DbSet<Products> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
     }
 }
