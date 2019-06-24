7cb359079f899d5a73642efc7eef86ba1fcf0a8d
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-24 01:50:16 -0600

    added migration for applicationUser model and table

--- GraniteWarehouse/Data/Migrations/20190624074838_AddedApplicationUser.Designer.cs
+++ GraniteWarehouse/Data/Migrations/20190624074838_AddedApplicationUser.Designer.cs
@@ -4,14 +4,16 @@ using GraniteWarehouse.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GraniteWarehouse.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190624074838_AddedApplicationUser")]
+    partial class AddedApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
