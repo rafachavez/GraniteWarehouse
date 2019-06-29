4007fc42de5e2e47c51e63502d8fe4784a836c85
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-29 12:18:15 -0600

    added post edit action fro adminUsers and added edit view

--- GraniteWarehouse/Areas/Admin/Controllers/AdminUsersController.cs
+++ GraniteWarehouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteWarehouse.Data;
+using GraniteWarehouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteWarehouse.Areas.Admin.Controllers
@@ -23,6 +24,48 @@ namespace GraniteWarehouse.Areas.Admin.Controllers
         }
 
         //Get Edit
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(id == null || id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+
+            var userFromDb = await _db.ApplicationUser.FindAsync(id);
+
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFromDb);
+        } 
+
+        //Post Edit
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(string id, ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser userFromDb = _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefault();
+                userFromDb.Name = applicationUser.Name;
+                userFromDb.PhoneNumber = applicationUser.PhoneNumber;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
+        }
 
     }
 }
